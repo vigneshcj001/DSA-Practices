d595953495b969d783b711ab1c7cc83716fde43a
vigneshcj001 <vigneshcj001@gmail.com>
2026-02-08 13:57:23 +0530

    Loops

---
+++ 1_Warm_Up/6_loops_functions.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+for (let i =1; i<3;i++){
+    greet("Vignesh");
+}
---
+++ 1_Warm_Up/7_loops_functions_with_different_names.js
@@ -0,0 +1,8 @@
+function greet(name){
+    console.log(`Hello ${name}`)
+}
+
+names = ["Vignesh","Vicky","Vigneshwaran"]
+for (let i=0; i<names.length;i++){
+    greet(names[i])
+}
